1fb4b52b19fc17dbf6b6c71835f09fee812c60bb
Igor Ryabchuk <riabtchuk@gmail.com>
2018-04-18 14:33:35 +0100

    rspec for attribute check

--- lib/DockingStation.rb
+++ lib/DockingStation.rb
@@ -1,11 +1,12 @@
 require_relative 'Bike'
 class DockingStation
+  attr_reader :bike
 
   def release_bike
     Bike.new
   end
 
   def dock_bike(bike)
-    bike
+    @bike = bike
   end
 end
--- spec/DockingStation_spec.rb
+++ spec/DockingStation_spec.rb
@@ -7,4 +7,8 @@ describe DockingStation do
   it "should give us the bike instance" do 
     expect(subject.dock_bike(bike)).to be_instance_of(Bike)
   end
+  it "should have an attribute bike instance" do
+    subject.dock_bike(bike)
+    expect(subject).to have_attributes(:bike => (be_instance_of(Bike)))
+  end
 end
